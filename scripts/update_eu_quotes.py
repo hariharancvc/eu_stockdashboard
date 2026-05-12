d1ab98daea79d5efba6cfb16dda2b455430302dd
hariharancv <hari.cruger@gmail.com>
2026-05-12 22:37:56 +0200

    updated the file access error

--- scripts/update_eu_quotes.py
+++ scripts/update_eu_quotes.py
@@ -9,7 +9,7 @@ API_KEY = os.environ["ALPHA_VANTAGE_API_KEY"]
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(os.path.dirname(SCRIPT_DIR))
-DATA_DIR = os.path.join(ROOT_DIR, "data")
+DATA_DIR = Path(ROOT_DIR) / "data"
 SYMBOLS_FILE = DATA_DIR / 'symbols.json'
 QUOTES_FILE = DATA_DIR / 'quotes.json'
 os.makedirs(DATA_DIR, exist_ok=True)
